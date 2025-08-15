4d91cbcb2c5312f16354bc6939d91e9d20b2cabc
Amit <ranjamit1@gmail.com>
2025-08-15 11:07:46 +0530

    Restore user-services after accidental deletion

---
+++ user-services/app/models/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from pydantic import EmailStr
+
+#request schemas for user
+class UserRequest(BaseModel):
+    email:str
+    password:str
+    nme:str
+
+#request schemas for UserLogin
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
+
+class TokenData(BaseModel):
+    _id:Optional[int]
+
+#to update user
+class UpdateUser(BaseModel):
+    nme:Optional[str]
+    email:Optional[EmailStr]
+    password:Optional[str]
