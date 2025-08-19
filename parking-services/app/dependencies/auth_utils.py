cb3fd6603108752b531027167a8f5abecd3e16ef
Amit <ranjamit1@gmail.com>
2025-08-19 20:33:24 +0530

    fix:database connection

--- parking-services/app/dependencies/auth_utils.py
+++ parking-services/app/dependencies/auth_utils.py
@@ -1,9 +1,8 @@
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt,JWTError
 from fastapi import Depends,HTTPException,status
-from app.config import SECRET_KEY,ALGORITHM
+from app.config import settings
 from typing import Dict,Any
-from datetime import datetime,timedelta
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -14,7 +13,7 @@ def get_token_payload(token:str=Depends(oauth2_scheme))->Dict[str,Any]:
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},)
     try:
-        payload=jwt.decode(token,SECRET_KEY,algorithms=ALGORITHM)
+        payload=jwt.decode(token,settings.SECRET_KEY,algorithms=settings.ALGORITHM)
         return payload
     except JWTError:
         raise credential_exception
@@ -28,3 +27,4 @@ def requires_role(required_role: str):
             )
         return payload
     return role_checker
+
