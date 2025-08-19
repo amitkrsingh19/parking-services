346f3495610d3690610c727ea13ad2084b5db7a2
Amit <ranjamit1@gmail.com>
2025-08-19 19:47:54 +0530

    Add CORS Middleware

--- gateway-api/app/main.py
+++ gateway-api/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app import api
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
+Origins=["http://localhost:8000",
+         "http://0.0.0.0:8000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+
+)
 app.include_router(api.router)
 
 
